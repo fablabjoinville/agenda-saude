bd686e0ff7badd519ddef2df4ea9019ecb8935b0
Pedro Germano Cervi <pedro@magrathealabs.com>
2020-04-04 19:11:19 -0300

    Revert home to patient_new

--- app/controllers/home_controller.rb
+++
@@ -1,3 +0,0 @@
-class HomeController < ActionController::Base
-  def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :patients, path: 'patients', controllers: { sessions: 'patients/sessions', registrations: 'patients/registrations' }
   devise_for :users, path: 'users', controllers: { sessions: 'users/sessions' }
 
-  root to: "home#index"
+  devise_scope :patient do
+    root to: 'patients/sessions#new'
+  end
 
   resources :appointments
 end
