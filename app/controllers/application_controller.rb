1862104ac01f6ce5cd31e71ae8ee123ec83ccd33
Lauro <lauro@magrathealabs.com>
2020-04-04 14:08:36 -0300

    Delete Devise folder and fix some i18n issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
+  def set_locale
+      I18n.locale = 'pt-BR'
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-class Patients::SessionsController < Devise::SessionsController
+class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # CPF logic
   # GET /resource/sign_in
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
