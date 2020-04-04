3963340fe01595a86880a4d974b00f80323c15c3
Pedro Germano Cervi <pedro@magrathealabs.com>
2020-04-04 11:37:41 -0300

    Added patient as devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :patients, path: 'patients'
+  devise_for :users, path: 'users'
 
-  # WARNING: WRONG ROUTE. Fix later,
-  # authenticated :user do
-  #   root '/', as: :authenticated_root
-  # end
-  devise_scope :user do
+  devise_scope :patient do
     root 'devise/sessions#new'
   end
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200404155338_create_appointments.rb
+++ db/migrate/20200404155338_create_appointments.rb
@@ -10,3 +10,4 @@ class CreateAppointments < ActiveRecord::Migration[6.0]
     end
   end
 end
+
