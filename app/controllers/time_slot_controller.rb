d3bb151273898d7dd009e941718ad99ec1a168c6
Gustavo Diel <gustavodiel@hotmail.com>
2020-04-05 14:27:12 -0300

    Allow patient to replace and cancel appointment

--- app/controllers/time_slot_controller.rb
+++ app/controllers/time_slot_controller.rb
@@ -3,13 +3,24 @@ class TimeSlotController < PatientSessionController
     @ubs = Ubs.find(schedule_params[:ubs_id])
     start_time = Time.parse(schedule_params[:start_time])
 
-    @appointment = Appointment.create(
-      patient: current_patient,
-      ubs: @ubs,
-      start: start_time,
-      end: start_time + @ubs.slot_interval,
-      active: true
-    )
+    Appointment.transaction do
+      if Appointment.where(start: start_time, ubs: @ubs).present?
+        flash[:notice] = 'Opa! O horário foi reservado enquanto você escolhia, tente outro!'
+        redirect_to time_slot_path
+
+        return
+      end
+
+      current_patient.appointments.destroy_all
+
+      @appointment = Appointment.create(
+        patient: current_patient,
+        ubs: @ubs,
+        start: start_time,
+        end: start_time + @ubs.slot_interval,
+        active: true
+      )
+    end
 
     return render json: @appointment.errors unless @appointment.save
 
@@ -17,12 +28,16 @@ class TimeSlotController < PatientSessionController
   end
 
   def cancel
+    @appointment = Appointment.find(cancel_params[:appointment_id])
 
+    @appointment.destroy
+
+    redirect_to time_slot_path
   end
 
   def index
     @appointment = current_patient.appointments.first
-    @ubs = @appointment.ubs
+    @ubs = @appointment.try(:ubs)
 
     @time_slots = Ubs.all.each_with_object({}) do |ubs, memo|
       memo[ubs] = ubs.available_time_slots(Date.today...3.days.from_now)
@@ -33,5 +48,9 @@ class TimeSlotController < PatientSessionController
 
   def schedule_params
     params.permit(:start_time, :ubs_id)
+  end
+
+  def cancel_params
+    params.permit(:appointment_id)
   end
 end
