4aae7f64eae2226b59abde84f9f811ce5f85d9b9
Pedro Germano Cervi <pedro@magrathealabs.com>
2020-04-06 22:43:40 -0300

    Prevent appointment overtake

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ class Appointment < ApplicationRecord
   belongs_to :ubs
 
   scope :active_from_day, ->(day) do
-    where(active: true).where('start >= ? AND appointments.end <= ?', day.beginning_of_day, day.end_of_day)
+    where('start >= ? AND appointments.end <= ?', day.beginning_of_day, day.end_of_day)
   end
 
   def active?
