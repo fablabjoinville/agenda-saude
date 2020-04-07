ba7c751ec204f074e0d4cef1506c4e66cc4e0f95
Mat-Bit <mateus.lbittencourt@gmail.com>
2020-04-07 14:52:16 -0300

    updated list of neighborhoods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_221733) do
+ActiveRecord::Schema.define(version: 2020_04_07_043047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_221733) do
 
   create_table "neighborhoods", force: :cascade do |t|
     t.string "name"
+    t.bigint "index", null: false
   end
 
   create_table "neighborhoods_ubs", id: false, force: :cascade do |t|
