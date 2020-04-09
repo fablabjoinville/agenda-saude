677e6e7613af75006b05acce4bae86eaf2f68fe1
Lauro <lauro@magrathealabs.com>
2020-04-09 20:52:26 -0300

    Simplify scripts

--- db/migrate/20200407043047_add_index_to_neighborhoods.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToNeighborhoods < ActiveRecord::Migration[6.0]
-  def change
-    add_column :neighborhoods, :index, :int, null: false
-  end
-end
--- scripts/populate.rb
+++ scripts/populate.rb
@@ -10,44 +10,47 @@ def create_ubs
     cnes = row[:cnes]
     hex = SecureRandom.hex
 
+    begin
+      user = User.create(
+        name: cnes,
+        password: hex,
+        password_confirmation: hex
+      )
+    rescue
+      puts "Usuario ja cadastrado."
+      next
+    end
+
     @users << { cnes: cnes, hex: hex }
 
-    user = User.create(
-      name: cnes,
-      password: hex,
-      password_confirmation: hex
-    )
-
-    Ubs.create(
-      name: row[:nome_unidade],
-      neighborhood: row[:bairro],
-      cnes: cnes,
-      phone: row[:telefone],
-      address: row[:endereco],
-      user: user,
-      shift_start: '9:00',
-      break_start: '12:30',
-      break_end: '13:30',
-      shift_end: '17:00',
-      slot_interval_minutes: 15,
-      active: false
-    )
+    begin
+      ubs = Ubs.create(
+        name: row[:nome_unidade],
+        neighborhood: row[:bairro],
+        cnes: cnes,
+        phone: row[:telefone],
+        address: row[:endereco],
+        user: user,
+        shift_start: '9:00',
+        break_start: '12:30',
+        break_end: '13:30',
+        shift_end: '17:00',
+        slot_interval_minutes: 15,
+        active: false
+      )
+    rescue
+      puts "UBS ja cadastrada."
+    end
   end
 end
 
 def create_relations
-  csv = SmarterCSV.process('scripts/bairros_com_id_x_ubs.csv')
+  csv = SmarterCSV.process('scripts/bairros_x_ubs.csv')
 
   csv.each do |row|
     ubs = Ubs.find_by(cnes: row[:cnes])
-
-    neighborhood_name = row[:bairro]
-    neighborhood_index = row[:id]
-
-    neighborhood = Neighborhood.find_by(index: neighborhood_index)
-    neighborhood ||= Neighborhood.create(name: neighborhood_name, index: neighborhood_index)
-
-    ubs.neighborhoods << neighborhood
+    ubs.neighborhoods << Neighborhood.find_or_create_by(name: row[:bairro])
+    ubs.neighborhoods = ubs.neighborhoods.uniq
     ubs.save!
   end
 end
