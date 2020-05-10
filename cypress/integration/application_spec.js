aa0f99ce1251332f899fd8c5ec93e5306d6cd4aa
Lauro <lauro@magrathealabs.com>
2020-05-09 22:22:36 -0300

    Improve flow and add test

--- cypress/integration/application_spec.js
+++ cypress/integration/application_spec.js
@@ -7,4 +7,37 @@ describe('Bedridden flow', () => {
 
     expect(true).to.equal(true)
   })
+
+	describe('user already exists and has appointment', function() {
+		it('requests home care, logs out and logs in again', function() {
+		//Login
+		cy.visit('/')
+
+		// Inform CPF and enter
+		cy.get('.row > .col > form > .form-group > #patient_cpf').click()
+		cy.get('.row > .col > form > .form-group > #patient_cpf').type('82920382640')
+		cy.get('.row > .col > form > .actions > .btn').click()
+
+		// Select correct mother name and enter
+		cy.get('.row > .col > #new_patient > .form-group > #patient_password').select('Num')
+		cy.get('.row > .col > #new_patient > .actions > .btn').click()
+
+		// Click on request home care
+		cy.get('.container > .alert > .col > .button_to > .btn').click()
+
+		// Logout
+		cy.get('body > .navbar > .button_to > .btn').click()
+
+		// Login again
+		cy.get('.row > .col > form > .form-group > #patient_cpf').click()
+		cy.get('.row > .col > form > .form-group > #patient_cpf').type('82920382640')
+		cy.get('.row > .col > form > .actions > .btn').click()
+
+		cy.get('.row > .col > #new_patient > .form-group > #patient_password').select('Num')
+		cy.get('.row > .col > #new_patient > .actions > .btn').click()
+
+		// Assert home care message
+		cy.get('body > .container > .row > .col > .h3').contains('Pronto! Atendimento em domicílio solicitado')
+		})
+	})
 })
