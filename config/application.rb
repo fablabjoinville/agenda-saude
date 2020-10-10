e4399fc9a07c9ca6621842fb1201a0325274415d
Alan Fachini <4171+alfakini@users.noreply.github.com>
2020-10-10 13:03:21 -0300

    Remove engines padrões (#78)
    * Unload default Rails engines
    
    * Update application.rb
    
    Co-authored-by: Alan Fachini <hi@alfakini.com>

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,26 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+
+# action_cable/engine
+# action_mailbox/engine
+# action_text/engine
+%w(
+  active_record/railtie
+  active_storage/engine
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
