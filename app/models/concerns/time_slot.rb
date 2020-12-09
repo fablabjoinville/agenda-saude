a3ac48f639697b29a4f3d4bd28830e1a3a7c0b7c
Mat-Bit <mateus.lbittencourt@gmail.com>
2020-12-09 00:34:41 -0300

    Secretaria: atualizar dias de feriado e banner informativo

--- app/models/concerns/time_slot.rb
+++ app/models/concerns/time_slot.rb
@@ -1,16 +1,12 @@
 module TimeSlot
-  OFFICIAL_HOLIDAYS = %w[01-01 10-04 20-04 21-04 01-05 11-06 12-06 07-09 12-10 13-10 28-10 02-11 15-11 21-12 22-12 23-12 24-12 25-12 28-12 29-12 30-12 31-12].freeze
+  OFFICIAL_HOLIDAYS = %w[01-01 02-01 10-04 20-04 21-04 01-05 11-06 12-06 07-09 12-10 13-10 28-10 02-11 15-11 19-12 21-12 22-12 23-12 24-12 25-12 26-12 28-12 29-12 30-12 31-12].freeze
 
   # TODO: Extract to helper
   def business_day?(day, open_sat)
     formated_day = day.strftime('%d-%m')
 
     if open_sat
-      if day.on_weekday?
-        OFFICIAL_HOLIDAYS.exclude?(formated_day) && day.on_weekday?
-      else
-        day.saturday?
-      end
+      OFFICIAL_HOLIDAYS.exclude?(formated_day) && (day.on_weekday? || day.saturday?)
     else
       OFFICIAL_HOLIDAYS.exclude?(formated_day) && day.on_weekday?
     end
