43bc679b8a8d6b7b732c5801f4cf9234c2db3496
t.schaiana.sonaglio <schaiana.sonaglio@magrathealabs.com>
2021-01-06 13:48:41 -0300

    Remove PhoneLib gem and add RegEx to validate phone number

--- app/validators/phone_format_validator.rb
+++ app/validators/phone_format_validator.rb
@@ -1,6 +1,6 @@
 class PhoneFormatValidator < ActiveModel::EachValidator #stardard lib rails
   def validate_each(record, attribute, value)
-    return if Phonelib.valid?(value)
+    return if value.match(/\([1-9]{2}\) (?:9[1-9][0-9]{3}|[1-9][0-9]{3})\-[0-9]{4}/)
 
     record.errors.add(attribute, :invalid)
   end
