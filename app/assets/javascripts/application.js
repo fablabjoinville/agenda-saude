9b9e867514ffed86792ef1d260f7c4a95b32ce20
t.schaiana.sonaglio <schaiana.sonaglio@magrathealabs.com>
2021-01-06 13:46:29 -0300

    Add optional number, when first 9 is missing or is not a mobile number

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,14 +3,15 @@
 //= require_tree .
 
 $(document).ready(function(){
- $('.sp_celphones').mask(SPMaskBehavior, spOptions);
+  $('.sp_celphones').mask(SPMaskBehavior, spOptions);
 });
 
 var SPMaskBehavior = function (val) {
-  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-0000';
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
 },
 spOptions = {
   onKeyPress: function(val, e, field, options) {
       field.mask(SPMaskBehavior.apply({}, arguments), options);
     }
 };
+
