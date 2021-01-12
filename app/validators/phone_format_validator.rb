b345921b9b4984da1332d2ecbf2a642428a9380e
Schaiana Sonaglio <31892261+schaiana@users.noreply.github.com>
2021-01-12 15:23:47 -0300

    Remove comment
    Co-authored-by: Iago Silva <iago.cith@hotmail.com>

--- app/validators/phone_format_validator.rb
+++ app/validators/phone_format_validator.rb
@@ -1,4 +1,4 @@
-class PhoneFormatValidator < ActiveModel::EachValidator #stardard lib rails
+class PhoneFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.match(/\([1-9]{2}\) (?:9[1-9][0-9]{3}|[1-9][0-9]{3})\-[0-9]{4}/)
 
