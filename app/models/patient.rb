67d6e8f329056369b271da46f46a4eef89f9fe26
Pedro Germano Cervi <pedro@magrathealabs.com>
2021-02-02 22:35:12 -0300

    Group display fix

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,7 +4,7 @@ class Patient < ApplicationRecord
   MAX_LOGIN_ATTEMPTS = 2
 
   CONDITIONS = {
-    # 'Trabalhador(a) da Saúde com mais de 60 anos' => ->(patient) { patient.in_group?('Trabalhador(a) da Saúde') && patient.age > 60 },
+    'Trabalhador(a) da Saúde com mais de 60 anos' => ->(patient) { patient.in_group?('Trabalhador(a) da Saúde') && patient.age > 300 },
     'Paciente de teste' => ->(patient) { patient.cpf == ENV['ROOT_PATIENT_CPF'] },
     # 'Maiores de 60 anos institucionalizadas' => ->(patient) { patient.age >= 60 && patient.in_group?('Institucionalizado(a)') },
     # 'População Indígena' => ->(patient) { patient.in_group?('Indígena') },
