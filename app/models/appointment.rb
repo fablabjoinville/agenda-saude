ce63f8cfb3eed176d6ed88e5d2c6cc3a62f141c1
Mateus Bittencourt <mateus.lbittencourt@gmail.com>
2021-02-26 14:05:40 -0300

    Update status appointment logic (#158)
    * Update status appointment logic
    
    * Update app/controllers/home_controller.rb
    
    Co-authored-by: Eduardo Fonseca <edrd.fons@gmail.com>
    
    * Refactoring the query to handle with multiple ubs
    
    Co-authored-by: Eduardo Fonseca <edrd.fons@gmail.com>

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :ubs
   belongs_to :patient, optional: true
 
+  SLOTS_WINDOW_IN_DAYS = ENV['SLOTS_WINDOW_IN_DAYS']&.to_i || 3
+
   scope :today, -> { where('date(start) = ?', Date.current) }
   scope :without_checkout, -> { where(check_out: nil) }
   scope :active_from_day, ->(day) do
@@ -16,5 +18,13 @@ class Appointment < ApplicationRecord
 
   def in_allowed_check_in_window?
     start > Time.zone.now.beginning_of_day && start < Time.zone.now.end_of_day
+  end
+
+  def self.free
+    joins(:ubs).where(ubs: { active: true }).where(patient_id: nil)
+  end
+
+  def self.within_allowed_window
+    where(start: Time.zone.now..(Time.zone.now + SLOTS_WINDOW_IN_DAYS.days).end_of_day)
   end
 end
