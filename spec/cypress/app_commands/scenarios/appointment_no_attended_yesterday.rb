fe29bc350a559f94d886869599b439518fb0de55
Eduardo Fonseca <edrd.fons@gmail.com>
2021-02-28 20:45:15 -0300

    Ensure scenario records are created

--- spec/cypress/app_commands/scenarios/appointment_no_attended_yesterday.rb
+++ spec/cypress/app_commands/scenarios/appointment_no_attended_yesterday.rb
@@ -1,6 +1,6 @@
 ubs = Ubs.find_by!(active: true)
 
-Appointment.create(
+Appointment.create!(
   start: Time.zone.yesterday,
   end: Time.zone.yesterday + 20.minutes,
   patient: Patient.find_by!(cpf: command_options['cpf']),
