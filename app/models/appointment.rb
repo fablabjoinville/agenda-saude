a65b75cf8ea052b6f8d7f54ff2319f30187ddb21
Julio Monteiro <julio@monteiro.eti.br>
2021-03-23 02:15:09 -0300

    New scopes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,16 +4,26 @@ class Appointment < ApplicationRecord
 
   SLOTS_WINDOW_IN_DAYS = ENV['SLOTS_WINDOW_IN_DAYS']&.to_i || 7
 
+  scope :start_between, -> (from, to) { where(start: from..to) }
+
+  # TODO: scope :today, -> { start_between(Time.zone.now.beginning_of_day, Time.zone.now.end_of_day) }
   scope :today, -> { where('date(start) = ?', Date.current) }
+
   scope :without_checkout, -> { where(check_out: nil) }
+
+  # TODO: remove me
   scope :active_from_day, ->(day) do
     where('start >= ? AND appointments.end <= ?', day.beginning_of_day, day.end_of_day)
   end
 
+  # TODO: scope :futures, -> { where(arel_table[:start].gt(Time.zone.now)) }
   scope :futures, -> { where('start > ?', Time.zone.now) }
 
   scope :free, -> { joins(:ubs).where(ubs: { active: true }).where(patient_id: nil) }
 
+  scope :active, -> { where(active: true) }
+
+  # TODO: remove me
   scope :within_allowed_window, -> {
     where(start: Time.zone.now..(Time.zone.now + SLOTS_WINDOW_IN_DAYS.days).end_of_day)
   }
