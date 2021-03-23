8dc9691ee2ea85096006f720476eaddeecc54b66
Pedro Germano Cervi <pedro@magrathealabs.com>
2021-03-23 08:43:32 -0300

    fix: better pagination and ordenation (#175)
    * fix: better pagination and ordenation
    
    * Update app/views/ubs/index.html.erb
    
    Co-authored-by: Eduardo Fonseca <edrd.fons@gmail.com>
    
    * Update app/views/ubs/index.html.erb
    
    Co-authored-by: Eduardo Fonseca <edrd.fons@gmail.com>

--- app/controllers/time_slot_controller.rb
+++ app/controllers/time_slot_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require_relative './../helpers/time_slot_helper'
 # include TimeSlotHelper
 
@@ -79,7 +80,7 @@ class TimeSlotController < PatientSessionController
 
         next unless appointments.exists?
 
-        appointments.each do |appointment|
+        appointments.order(:start).each do |appointment|
           slots << { slot_start: appointment.start, slot_end: appointment.end }
         end
 
