17c13892dc83389b4f6987f09fa9a48e31af92b3
Julio Monteiro <julio@monteiro.eti.br>
2021-03-22 23:37:23 -0300

    Use — instead of - and remove trailing .

--- app/models/ubs.rb
+++ app/models/ubs.rb
@@ -13,7 +13,7 @@ class Ubs < ApplicationRecord
   scope :active, -> { where(active: true) }
 
   def identifier
-    "#{name} - #{address}."
+    "#{name} — #{address}"
   end
 
   def shift_start_date(date = Date.today)
