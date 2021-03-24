2e83b58bc67c61b8ccbe21b3771e94d82139e6c3
Julio Monteiro <julio@monteiro.eti.br>
2021-03-24 10:48:24 -0300

    Rename vars to make it more clear

--- app/services/appointment_scheduler.rb
+++ app/services/appointment_scheduler.rb
@@ -14,7 +14,7 @@ class AppointmentScheduler
     return [:invalid_schedule_time] if start_time > (Time.zone.now + @max_schedule_time_ahead).at_end_of_day
     return [:schedule_conditions_unmet] unless patient.can_schedule?
 
-    current_appointment = patient.appointments.future.current
+    future_appointment = patient.appointments.future.current
 
     # Single SQL query to update the first available record it can find
     # it will either return 0 if no rows could be found, or 1 if it was able to schedule an appointment
@@ -25,19 +25,19 @@ class AppointmentScheduler
 
     return [:all_slots_taken] if rows_updated.zero?
 
-    appointment = patient.appointments.where.not(id: current_appointment&.id).find_by!(ubs: ubs, start: start_time)
-    if current_appointment
+    new_appointment = patient.appointments.where.not(id: future_appointment&.id).find_by!(ubs: ubs, start: start_time)
+    if future_appointment
       # Update new appointment with data from current
-      appointment.update!(
-        second_dose: current_appointment.second_dose,
-        vaccine_name: current_appointment.vaccine_name
+      new_appointment.update!(
+        second_dose: future_appointment.second_dose,
+        vaccine_name: future_appointment.vaccine_name
       )
 
-      # Free up current appointment
-      current_appointment.update!(patient: nil)
+      # Free up future appointment
+      future_appointment.update!(patient: nil)
     end
 
-    [:success, appointment]
+    [:success, new_appointment]
   rescue => e
     ExceptionNotifierService.(e)
 
