292460b5950f4c5b6eba86ff8719742aa3c0850d
Julio Monteiro <julio@monteiro.eti.br>
2021-03-25 13:30:48 -0300

    Set default to true to active to avoid creation of suspended appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_015841) do
+ActiveRecord::Schema.define(version: 2021_03_25_161545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_015841) do
   create_table "appointments", force: :cascade do |t|
     t.datetime "start"
     t.datetime "end"
-    t.boolean "active"
+    t.boolean "active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "ubs_id"
