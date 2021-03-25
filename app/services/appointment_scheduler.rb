21772d9a5256488501dcd50221052c7bca997699
Julio Monteiro <julio@monteiro.eti.br>
2021-03-25 09:05:36 -0300

    Re-introduce Appointment.transaction

--- app/services/appointment_scheduler.rb
+++ app/services/appointment_scheduler.rb
@@ -14,27 +14,30 @@ class AppointmentScheduler
     return [:invalid_schedule_time] if start_time > (Time.zone.now + @max_schedule_time_ahead).at_end_of_day
     return [:schedule_conditions_unmet] unless patient.can_schedule?
 
+    new_appointment = nil
     future_appointment = patient.appointments.future.current
 
-    # Single SQL query to update the first available record it can find
-    # it will either return 0 if no rows could be found, or 1 if it was able to schedule an appointment
-    rows_updated = Appointment.
-      where(start: start_time, ubs_id: ubs, patient_id: nil).
-      limit(1).
-      update_all(patient_id: patient.id)
+    Appointment.transaction(isolation: :repeatable_read) do
+      # Single SQL query to update the first available record it can find
+      # it will either return 0 if no rows could be found, or 1 if it was able to schedule an appointment
+      rows_updated = Appointment.
+        where(start: start_time, ubs_id: ubs, patient_id: nil).
+        limit(1).
+        update_all(patient_id: patient.id)
 
-    return [:all_slots_taken] if rows_updated.zero?
+      return [:all_slots_taken] if rows_updated.zero?
 
-    new_appointment = patient.appointments.where.not(id: future_appointment&.id).find_by!(ubs: ubs, start: start_time)
-    if future_appointment
-      # Update new appointment with data from current
-      new_appointment.update!(
-        second_dose: future_appointment.second_dose,
-        vaccine_name: future_appointment.vaccine_name
-      )
+      new_appointment = patient.appointments.where.not(id: future_appointment&.id).find_by!(ubs: ubs, start: start_time)
+      if future_appointment
+        # Update new appointment with data from current
+        new_appointment.update!(
+          second_dose: future_appointment.second_dose,
+          vaccine_name: future_appointment.vaccine_name
+        )
 
-      # Free up future appointment
-      future_appointment.update!(patient: nil)
+        # Free up future appointment
+        future_appointment.update!(patient: nil)
+      end
     end
 
     [:success, new_appointment]
