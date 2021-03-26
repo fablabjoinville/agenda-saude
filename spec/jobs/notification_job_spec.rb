32f40ba22a6f470d3ac2c329ddd9bbfed6963e5c
Julio Monteiro <julio@monteiro.eti.br>
2021-03-26 19:51:39 -0300

    Additional test

--- spec/jobs/notification_job_spec.rb
+++ spec/jobs/notification_job_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe NotificationJob, type: :job do
       .with('info', 'this is an info')
       .on_queue('default')
   end
+
+  it 'executes perform' do
+    expect_any_instance_of(described_class).to receive(:send_message).with('ℹ️ this is an info')
+    perform_enqueued_jobs { job }
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
 end
