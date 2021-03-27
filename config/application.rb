7651e4ad9bdf8f1a1312ff1b70eb891eb237a66f
Julio Monteiro <julio@monteiro.eti.br>
2021-03-27 01:38:16 -0300

    Revert "Disabling and removing ActionMailer and ActiveJob"
    This reverts commit a729c7a0e6631aed798500e85fb337b592403499.

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
