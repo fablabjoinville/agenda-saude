caef66fafd2389a30c5b07151db0fc576473edbe
Julio Monteiro <julio@monteiro.eti.br>
2021-03-28 12:22:02 -0300

    Refreshed home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,17 @@
 class HomeController < ApplicationController
   def index
-    return redirect_to index_bedridden_path if current_patient.try(:bedridden?)
+    return redirect_to index_bedridden_path if current_patient&.bedridden?
     return redirect_to home_community_appointments_path if current_patient
     return redirect_to operator_appointments_path if current_user
 
     from = Rails.configuration.x.schedule_from_hours.hours.from_now
     to = Rails.configuration.x.schedule_up_to_days.days.from_now.end_of_day
-    @is_scheduling_available = Appointment.active_ubs
-                                          .active
-                                          .not_scheduled
-                                          .open
-                                          .where(start: from..to)
-                                          .any?
+    @available_appointments_count = Appointment.active_ubs
+                                               .active
+                                               .not_scheduled
+                                               .open
+                                               .where(start: from..to)
+                                               .count
   end
 
   def patient_base_login
