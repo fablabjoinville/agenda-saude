64608b5456b5f317467a1b89d8f51a3a6444f203
Julio Monteiro <julio@monteiro.eti.br>
2021-03-29 00:25:13 -0300

    Document code

--- app/controllers/community/appointments_controller.rb
+++ app/controllers/community/appointments_controller.rb
@@ -1,9 +1,5 @@
 module Community
   class AppointmentsController < Base
-    class NoFreeAppointments < StandardError; end
-
-    ROUNDING = 2.minutes # delta to avoid rounding issues
-
     def home
       return redirect_to(vaccinated_community_appointments_path) if current_patient.vaccinated?
 
@@ -25,69 +21,24 @@ module Community
       return redirect_to(home_community_appointments_path) unless @appointment
 
       @days = parse_days
-      from = [
-        @days.days.from_now.beginning_of_day,
-        Rails.configuration.x.schedule_from_hours.hours.from_now
-      ].max
-      to = @days.days.from_now.end_of_day
-
-      @appointments = Appointment.active_ubs
-                                 .active
-                                 .not_scheduled
-                                 .open
-                                 .where(start: from..to)
-                                 .order(:start) # in chronological order
-                                 .select(:ubs_id, :start, :end) # only return what we care with
-                                 .distinct # remove duplicates (same as .uniq in pure Ruby)
-                                 .group_by(&:ubs) # transforms it into a Hash grouped by Ubs
-    rescue NoFreeAppointments
+      @appointments = scheduler.open_times_per_ubs(from: @days.days.from_now.beginning_of_day,
+                                                   to: @days.days.from_now.end_of_day)
+    rescue AppointmentScheduler::NoFreeSlotsAhead
       redirect_to home_community_appointments_path, flash: { alert: 'Não há vagas disponíveis para reagendamento.' }
     end
 
     def create
-      start = [
-        Rails.configuration.x.schedule_from_hours.hours.from_now,
-        parse_start
-      ].compact.max - ROUNDING
-
-      result, data = AppointmentScheduler.new.schedule(
+      result, appointment = scheduler.schedule(
         patient: current_patient,
         ubs_id: params[:ubs_id].presence,
-        start: start..(Rails.configuration.x.schedule_up_to_days.days.from_now.end_of_day)
+        from: parse_start
       )
 
-      case result
-      when AppointmentScheduler::CONDITIONS_UNMET
-        redirect_to home_community_appointments_path, flash: {
-          alert: 'Você não está entre grupos que podem fazer agendamentos.'
-        }
-      when AppointmentScheduler::NO_SLOTS
-        redirect_to home_community_appointments_path, flash: {
-          alert: 'Desculpe, mas não há mais vagas disponíveis. Tente novamente mais tarde.'
-        }
-      when AppointmentScheduler::SUCCESS
-        notice = if (start - data.start).abs > ROUNDING
-                   "O primeiro horário disponível para agendamento foi #{I18n.l data.start, format: :short}."
-                 else
-                   'Vacinação agendada para o horário desejado.'
-                 end
-
-        redirect_to home_community_appointments_path, flash: {
-          notice: notice
-        }
-      else
-        notify_unexpected_result(result: result, data: data, context: context)
-        redirect_to home_community_appointments_path, flash: {
-          alert: 'Ocorreu um erro inesperado, tente novamente mais tarde.'
-        }
-      end
+      redirect_to home_community_appointments_path, flash: message_for(result, appointment)
     end
 
     def destroy
-      current_patient.appointments
-                     .open
-                     .where(id: params[:id])
-                     .update_all(patient_id: nil)
+      scheduler.cancel_schedule(patient: current_patient, id: params[:id])
 
       redirect_to home_community_appointments_path
     end
@@ -102,39 +53,59 @@ module Community
 
     private
 
+    def scheduler
+      AppointmentScheduler.new(
+        earliest_allowed: Rails.configuration.x.schedule_from_hours.hours.from_now,
+        latest_allowed: Rails.configuration.x.schedule_up_to_days.days.from_now.end_of_day
+      )
+    end
+
+    def message_for(result, appointment)
+      case result
+      when AppointmentScheduler::CONDITIONS_UNMET
+        {
+          alert: 'Você não está entre grupos que podem fazer agendamentos.'
+        }
+      when AppointmentScheduler::NO_SLOTS
+        {
+          alert: 'Desculpe, mas não há mais vagas disponíveis. Tente novamente mais tarde.'
+        }
+      when AppointmentScheduler::SUCCESS
+        {
+          notice: success_message(start, appointment.start)
+        }
+      else
+        raise "Unexpected result #{result}"
+      end
+    end
+
+    def success_message(desired_date, scheduled_date)
+      if (desired_date - scheduled_date).abs > ROUNDING
+        "O primeiro horário disponível para agendamento foi #{I18n.l scheduled_date, format: :short}."
+      else
+        'Vacinação agendada para o horário desejado.'
+      end
+    end
+
     # Loads pages for the Index, between 0 and max possible allowed
     def parse_days
       [
         [
           0,
-          params[:page].presence&.to_i || open_not_scheduled_appointment_days_ahead
+          params[:page].presence&.to_i || scheduler.days_ahead_with_open_slot
         ].compact.max,
         Rails.configuration.x.schedule_up_to_days
       ].min
     end
 
-    # Returns how many days ahead there are available appointments
-    def open_not_scheduled_appointment_days_ahead
-      from = Rails.configuration.x.schedule_from_hours.hours.from_now
-      to = Rails.configuration.x.schedule_up_to_days.days.from_now.end_of_day
-
-      next_available_appointment = Appointment.active_ubs
-                                              .open
-                                              .not_scheduled
-                                              .where(start: from..to)
-                                              .order(:start)
-                                              .pick(:start)
-
-      raise NoFreeAppointments unless next_available_appointment
-
-      ((next_available_appointment - Time.zone.now.end_of_day) / 1.day).ceil
-    end
-
+    # rubocop:disable Rails/Date (as we're generating the string it with timezone)
     def parse_start
       create_params[:appointment]&.key?(:start) && create_params[:appointment][:start]&.to_time
     rescue ArgumentError
       nil
     end
+
+    # rubocop:enable Rails/Date
 
     def create_params
       params.permit(appointment: %i[ubs_id start])
--- app/services/appointment_scheduler.rb
+++ app/services/appointment_scheduler.rb
@@ -1,9 +1,28 @@
 class AppointmentScheduler
+  ROUNDING = 2.minutes # delta to avoid rounding issues
+
   CONDITIONS_UNMET = :conditions_unmet
   NO_SLOTS = :no_slots
   SUCCESS = :success
 
-  def schedule(patient:, ubs_id:, start:)
+  class NoFreeSlotsAhead < StandardError; end
+
+  attr_reader :earliest_allowed, :latest_allowed
+
+  # Scheduler for appointments, enabling appointments to be made between +earliest_allowed+ and +latest_allowed+ dates
+  def initialize(earliest_allowed:, latest_allowed:)
+    @earliest_allowed = earliest_allowed
+    @latest_allowed = latest_allowed
+  end
+
+  # Schedules an appointment for +patient+ starting +from+ a given timestamp, optionally with a +ubs_id+ (which can be
+  # nil in case we want to schedule on any Ubs available to the +patient+).
+  # It will schedule on the first possible time until the +latest_allowed+.
+  # Checks if the user can schedule, otherwise returns +CONDITIONS_UNMET+.
+  # Looks for appointments, and tries to schedule one. If it can't, it will return +NO_SLOTS+.
+  # In case it can, it will also cancel the patient's current schedule for an existing appointment, and in the end it
+  # returns +SUCCESS+ and the newly scheduled appointment.
+  def schedule(patient:, ubs_id:, from:)
     return [CONDITIONS_UNMET] unless patient.can_schedule?
 
     current_appointment = patient.appointments.current
@@ -12,7 +31,7 @@ class AppointmentScheduler
       success = update_appointment(
         patient_id: patient.id,
         ubs_id: ubs_id,
-        start: start
+        start: rounded_from(from)..latest_allowed
       )
       return [NO_SLOTS] unless success
 
@@ -33,6 +52,49 @@ class AppointmentScheduler
     end
   end
 
+  # Cancels schedule for an appointment for a given user, in a SQL efficient way
+  def cancel_schedule(patient:, id:)
+    patient.appointments
+           .open
+           .where(id: id)
+           .update_all(patient_id: nil, updated_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
+  end
+
+  def open_times_per_ubs(from:, to:)
+    from = [from, earliest_allowed].compact.max - ROUNDING
+
+    Appointment.active_ubs
+               .active
+               .not_scheduled
+               .open
+               .where(start: rounded_from(from)..to)
+               .order(:start) # in chronological order
+               .select(:ubs_id, :start, :end) # only return what we care with
+               .distinct # remove duplicates (same as .uniq in pure Ruby)
+               .group_by(&:ubs) # transforms it into a Hash grouped by Ubs
+  end
+
+  # Returns how many days ahead there are available appointments
+  def days_ahead_with_open_slot
+    next_available_appointment = Appointment.active_ubs
+                                            .open
+                                            .not_scheduled
+                                            .where(start: earliest_allowed..latest_allowed)
+                                            .order(:start)
+                                            .pick(:start)
+
+    raise NoFreeSlotsAhead unless next_available_appointment
+
+    ((next_available_appointment - Time.zone.now.end_of_day) / 1.day).ceil
+  end
+
+  protected
+
+  # Checks if we can schedule +from+ that time and then rounds to avoid issues with time conversion
+  def rounded_from(from)
+    [earliest_allowed, from].compact.max - ROUNDING
+  end
+
   def update_appointment(patient_id:, start:, ubs_id:)
     # Single SQL query to update the first available record it can find
     # it will either return 0 if no rows could be found, or 1 if it was able to schedule an appointment
@@ -47,7 +109,7 @@ class AppointmentScheduler
     appointment = appointment.where(ubs_id: ubs_id) if ubs_id.present?
 
     appointment
-      .update_all(patient_id: patient_id)
+      .update_all(patient_id: patient_id, updated_at: Time.zone.now) # rubocop:disable Rails/SkipsModelValidations
       .positive? # 0 = false, 1 = true
   end
 end
