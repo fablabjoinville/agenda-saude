5fb690bd74bca0061cda2f680fd47aebffb7e10b
Julio Monteiro <julio@monteiro.eti.br>
2021-03-29 12:41:14 -0300

    Fix redirect

--- app/controllers/operator/ubs_controller.rb
+++ app/controllers/operator/ubs_controller.rb
@@ -5,13 +5,13 @@ module Operator
     def activate
       @ubs.update!(active: true)
 
-      redirect_to[:operator, @ubs]
+      redirect_to operator_ubs_path(id: @ubs.id)
     end
 
     def suspend
       @ubs.update!(active: false)
 
-      redirect_to[:operator, @ubs]
+      redirect_to operator_ubs_path(id: @ubs.id)
     end
   end
 end
