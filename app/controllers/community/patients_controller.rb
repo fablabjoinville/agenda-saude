336e801e2a7af4099d4a1c6cc746cf2cd64c8552
Julio Monteiro <julio@monteiro.eti.br>
2021-03-29 11:25:19 -0300

    Use group_ids instead of manually handling groups

--- app/controllers/community/patients_controller.rb
+++ app/controllers/community/patients_controller.rb
@@ -5,7 +5,6 @@ module Community
     FIELDS = %i[
       birthday
       email
-      groups
       mother_name
       name
       neighborhood
@@ -25,10 +24,6 @@ module Community
     def create
       @patient = Patient.new create_params
 
-      # FIXME: use group_ids instead
-      @patient.target_audience = Patient.target_audiences["without_target"]
-      @patient.groups = Group.where id: params.require(:patient).permit(groups: [])['groups']
-
       if @patient.save
         session[:patient_id] = @patient.id
         redirect_to home_community_appointments_path
@@ -44,10 +39,6 @@ module Community
     def update
       @patient = current_patient
 
-      # FIXME
-      @patient.target_audience = Patient.target_audiences["without_target"]
-      @patient.groups = Group.where id: params.require(:patient).permit(groups: [])['groups']
-
       if @patient.update(update_params)
         redirect_to home_community_appointments_path, flash: {
           notice: 'Cadastro atualizado.'
@@ -60,11 +51,11 @@ module Community
     protected
 
     def create_params
-      params.require(:patient).permit(*(FIELDS + [:cpf]))
+      params.require(:patient).permit(*(FIELDS + [:cpf]), group_ids: [])
     end
 
     def update_params
-      params.require(:patient).permit(*FIELDS)
+      params.require(:patient).permit(*FIELDS, group_ids: [])
     end
   end
 end
