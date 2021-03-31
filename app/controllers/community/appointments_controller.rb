9cf0a24da9fb868b1f3e63fd7edbe5df0fb48e3f
Julio Monteiro <julio@monteiro.eti.br>
2021-03-30 21:06:57 -0300

    Improved message

--- app/controllers/community/appointments_controller.rb
+++ app/controllers/community/appointments_controller.rb
@@ -80,7 +80,7 @@ module Community
         }
       when AppointmentScheduler::NO_SLOTS
         {
-          alert: 'Desculpe, mas não há mais vagas disponíveis. Tente novamente mais tarde.'
+          alert: 'Desculpe, mas não foi possível agendar. Tente novamente mais tarde.'
         }
       when AppointmentScheduler::SUCCESS
         {
