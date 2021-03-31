009b69214097740f482584eb20a69b12f0d6b321
Julio Monteiro <julio@monteiro.eti.br>
2021-03-31 11:48:59 -0300

    rubocop

--- app/helpers/admin/patients_helper.rb
+++ app/helpers/admin/patients_helper.rb
@@ -3,32 +3,16 @@ module Admin
     LINKS = %i[all locked].freeze
 
     def admin_nav_links(current_filter, current_search, total_count)
-      admin_filter_tabs_links(current_filter, total_count).tap do |links|
-        links << search_link(current_search, total_count) if current_search.present?
+      filter_tabs_links(
+        current_filter: current_filter,
+        total_count: total_count,
+        links: LINKS,
+        filters: Admin::PatientsController::FILTERS,
+        i18n_scope: :patients,
+        path: ->(args) { admin_patients_path(**args) }
+      ).tap do |links|
+        links << search_link(current_search, total_count, admin_patients_path(search: current_search)) if current_search.present?
       end.join.html_safe # rubocop:disable Rails/OutputSafety
-    end
-
-    def admin_filter_tabs_links(current_filter, total_count)
-      LINKS.map do |key|
-        filter = Admin::PatientsController::FILTERS[key]
-        active = filter == current_filter
-        text = t("patients.state.#{key}")
-        text << " (#{total_count})" if active
-
-        tag.li(class: 'nav-item') do
-          link_to text, admin_patients_path(filter: filter), data: { cy: "#{filter}ListTab" },
-                                                             class: "nav-link #{active && :active}"
-        end
-      end
-    end
-
-    def admin_search_link(current_search, total_count)
-      tag.li(class: 'nav-item') do
-        link_to "Busca: #{current_search} (#{total_count})",
-                admin_patients_path(search: current_search),
-                class: 'nav-link active',
-                data: 'searchListTab'
-      end
     end
   end
 end
--- app/helpers/operator/appointments_helper.rb
+++ app/helpers/operator/appointments_helper.rb
@@ -3,32 +3,19 @@ module Operator
     LINKS = %i[all waiting checked_in checked_out].freeze
 
     def operator_nav_links(current_filter, current_search, total_count)
-      filter_tabs_links(current_filter, total_count).tap do |links|
-        links << search_link(current_search, total_count) if current_search.present?
-      end.join.html_safe # rubocop:disable Rails/OutputSafety
-    end
-
-    def filter_tabs_links(current_filter, total_count)
-      LINKS.map do |key|
-        filter = Operator::AppointmentsController::FILTERS[key]
-        active = filter == current_filter
-        text = t("appointments.state.#{key}")
-        text << " (#{total_count})" if active
-
-        tag.li(class: 'nav-item') do
-          link_to text, operator_appointments_path(filter: filter), data: { cy: "#{filter}ListTab" },
-                                                                    class: "nav-link #{active && :active}"
+      filter_tabs_links(
+        current_filter: current_filter,
+        total_count: total_count,
+        links: LINKS,
+        filters: Operator::AppointmentsController::FILTERS,
+        i18n_scope: :appointments,
+        path: ->(args) { operator_appointments_path(**args) }
+      ).tap do |links|
+        if current_search.present?
+          links << search_link(current_search, total_count,
+                               operator_appointments_path(search: current_search))
         end
-      end
-    end
-
-    def search_link(current_search, total_count)
-      tag.li(class: 'nav-item') do
-        link_to "Busca: #{current_search} (#{total_count})",
-                operator_appointments_path(search: current_search),
-                class: 'nav-link active',
-                data: 'searchListTab'
-      end
+      end.join.html_safe # rubocop:disable Rails/OutputSafety
     end
   end
 end
