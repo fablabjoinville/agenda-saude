50b8d58fbb70f8ccb02866ffccb8cff1a1b6af5e
Julio Monteiro <julio@monteiro.eti.br>
2021-04-01 22:13:51 -0300

    Remove legacy time slot code (#273)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,20 +15,6 @@ module ApplicationHelper
     "#{cpf[0..2]}.#{cpf[3..5]}.#{cpf[6..8]}-#{cpf[9..11]}"
   end
 
-  def business_day?(day, open_sat)
-    formated_day = day.strftime('%d-%m')
-
-    if open_sat
-      if day.on_weekday?
-        OFFICIAL_HOLIDAYS.exclude?(formated_day) && day.on_weekday?
-      else
-        day.saturday?
-      end
-    else
-      OFFICIAL_HOLIDAYS.exclude?(formated_day) && day.on_weekday?
-    end
-  end
-
   def search_link(current_search, total_count, path)
     tag.li(class: 'nav-item') do
       link_to "Busca: #{current_search} (#{total_count})",
