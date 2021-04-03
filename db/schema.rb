863a5495e1a14ef98e745b70cbc1b24d6e523e94
Julio Monteiro <julio@monteiro.eti.br>
2021-04-03 14:16:50 -0300

    Administrator flag for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_171135) do
+ActiveRecord::Schema.define(version: 2021_04_03_171540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 2021_04_03_171135) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", default: "", null: false
+    t.boolean "administrator", default: false, null: false
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
