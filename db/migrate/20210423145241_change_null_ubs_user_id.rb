e59ab04b2c348eaacf4b967cd27bb5fc890a29b8
Julio Monteiro <julio@monteiro.eti.br>
2021-04-23 12:01:47 -0300

    Adicionando rollback

--- db/migrate/20210423145241_change_null_ubs_user_id.rb
+++ db/migrate/20210423145241_change_null_ubs_user_id.rb
@@ -1,5 +1,9 @@
 class ChangeNullUbsUserId < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :ubs, :user_id, :bigint, null: true, default: nil
+  end
+
+  def down
+    change_column :ubs, :user_id, :bigint, null: false
   end
 end
