2621b4ce53403d2301617a3b548803c8e51426e9
Julio Monteiro <julio@monteiro.eti.br>
2021-04-22 23:56:11 -0300

    rubocop

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -2,10 +2,11 @@ module Admin
   class AppointmentsController < Base
     def index
       if index_params[:date].is_a? Hash
-        @date = Date.iso8601 [index_params[:date][1], index_params[:date][2].to_s.rjust(2, '0'), index_params[:date][3].to_s.rjust(2, '0')].join('-')
+        @date = Date.iso8601 [index_params[:date][1], index_params[:date][2].to_s.rjust(2, '0'),
+                              index_params[:date][3].to_s.rjust(2, '0')].join('-')
       end
 
-      @date = Date.today unless @date
+      @date ||= Date.today
 
       @appoitments = Appointment.where(ubs_id: index_params[:ubs_id], start: @date.beginning_of_day..@date.end_of_day)
     end
@@ -13,7 +14,7 @@ module Admin
     private
 
     def index_params
-      params.permit(:ubs_id, date: [:day, :month, :year])
+      params.permit(:ubs_id, date: %i[day month year])
     end
   end
 end
--- app/controllers/admin/ubs_controller.rb
+++ app/controllers/admin/ubs_controller.rb
@@ -2,8 +2,8 @@ module Admin
   class UbsController < Base
     def index
       @ubs = Ubs.order(Ubs.arel_table[:name].lower.asc)
-                   .page(index_params[:page])
-                   .per(25)
+                .page(index_params[:page])
+                .per(25)
     end
 
     def show
