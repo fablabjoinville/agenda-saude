f6935a18cfb2ca5a6fb9d44f9b37f49434cff87e
Julio Monteiro <julio@monteiro.eti.br>
2021-04-24 11:08:41 -0300

    Proteção, verifica permissão de administrador

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,18 @@
 module Admin
   class Base < ApplicationController
     before_action :authenticate_user!
+    before_action :require_administrator!
+
+    class AdministratorRequired < StandardError; end
+
+    rescue_from AdministratorRequired do |exception|
+      render plain: :forbidden, status: :forbidden
+    end
+
+    private
+
+    def require_administrator!
+      current_user.administrator || raise(AdministratorRequired)
+    end
   end
 end
