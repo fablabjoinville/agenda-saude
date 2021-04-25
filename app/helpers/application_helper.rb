94353b62221b36354f374c21ed5f2b7b22fb9fd5
Julio Monteiro <julio@monteiro.eti.br>
2021-04-25 19:31:40 -0300

    Remover metodos nao usados

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,6 @@ module ApplicationHelper
     date.strftime('%d/%m/%Y às %H:%M')
   end
 
-  def self.humanize_date(date)
-    date.strftime('%d/%m/%Y')
-  end
-
-  def self.humanize_time(date)
-    date.strftime('%H:%M')
-  end
-
   def self.humanize_cpf(cpf)
     "#{cpf[0..2]}.#{cpf[3..5]}.#{cpf[6..8]}-#{cpf[9..11]}"
   end
