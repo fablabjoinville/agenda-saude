d314f95ce46302477a7424b877a639aab75d4f9a
Julio Monteiro <julio@monteiro.eti.br>
2021-05-02 15:15:50 -0300

    Exemplo de comorbidades

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,18 +2,30 @@ class Patient < ApplicationRecord
   MAX_LOGIN_ATTEMPTS = 3
 
   CONDITIONS = {
-    'População em geral com 60 anos ou mais' => lambda do |patient|
+    'População em geral com 60 anos ou mais' =>
+    lambda do |patient|
       birthday = patient.birthday.to_time # rubocop:disable Rails/Date timezone is respected
       cutoff = Time.zone.now
       age = ((cutoff - birthday) / 1.year.seconds).floor
       age >= 60
     end
-    # 'Trabalhadores da saúde segundo OFÍCIO Nº 234/2021/CGPNI/DEIDT/SVS/MS' =>
-    # ->(patient) { patient.in_group?('Trabalhador(a) da Saúde') },
-    # 'Paciente de teste' => ->(patient) { patient.cpf == ENV['ROOT_PATIENT_CPF'] },
-    # 'Maiores de 60 anos institucionalizadas' =>
-    #   ->(patient) { patient.age >= 60 && patient.in_group?('Institucionalizado(a)') },
-    # 'População Indígena' => ->(patient) { patient.in_group?('Indígena') },
+    # 'População com 18 anos ou mais com uma das seguintes comorbidades: ' \
+    # 'Imunossuprimidos, Doenças cardiovasculares ou Hipertensão' =>
+    # lambda do |patient|
+    #   birthday = patient.birthday.to_time # rubocop:disable Rails/Date timezone is respected
+    #   cutoff = Time.zone.now
+    #   age = ((cutoff - birthday) / 1.year.seconds).floor
+    #
+    #   # Notes:
+    #   # - Não incluir nenhum com active=false
+    #   # - Pegar IDs de production para ter certeza
+    #   # - Quando aditionar um grupo com vários filhos, incluir todos os filhos
+    #   # - Quando quiser só adicionar uma subcondição espeçifica, só adicione o item (sem incluir o ID do grupo pai)
+    #   group_ids = [38, 68, 69, 70, 39, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 42, 88, 89, 90, 91]
+    #
+    #   # age check && check if there's an intersection between arrays
+    #   age >= 18 && (group_ids & patient.group_ids).any?
+    # end
   }.freeze
 
   has_many :appointments, dependent: :destroy do
@@ -66,10 +78,6 @@ class Patient < ApplicationRecord
       .future
       .active
       .any?
-  end
-
-  def in_group?(name)
-    groups.map(&:name).include?(name)
   end
 
   # Until we have a proper way to remember vaccines for patients
