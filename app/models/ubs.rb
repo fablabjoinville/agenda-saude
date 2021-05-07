35670bfe71d9b306a4685601280b9125973320f9
Julio Monteiro <julio@monteiro.eti.br>
2021-05-07 16:24:28 -0300

    rubocop

--- app/models/ubs.rb
+++ app/models/ubs.rb
@@ -4,10 +4,9 @@ class Ubs < ApplicationRecord
   validates :appointments_per_time_slot, numericality: { greater_than: 0 }
 
   belongs_to :user
-  has_many :appointments, dependent: :destroy
   has_and_belongs_to_many :neighborhoods
   has_and_belongs_to_many :users # For future use [jmonteiro]
-  has_one :time_slot_generation_config, dependent: :delete
+  has_many :appointments, dependent: :destroy
 
   scope :active, -> { where(active: true) }
 
@@ -19,35 +18,35 @@ class Ubs < ApplicationRecord
     "#{address} (#{neighborhood})"
   end
 
-  def shift_start_date(date = Date.today)
+  def shift_start_date(date = Time.zone.today)
     time_of_day(shift_start, date)
   end
 
-  def shift_end_date(date = Date.today)
+  def shift_end_date(date = Time.zone.today)
     time_of_day(shift_end, date)
   end
 
-  def break_start_date(date = Date.today)
+  def break_start_date(date = Time.zone.today)
     time_of_day(break_start, date)
   end
 
-  def break_end_date(date = Date.today)
+  def break_end_date(date = Time.zone.today)
     time_of_day(break_end, date)
   end
 
-  def shift_start_saturday(date = Date.today)
+  def shift_start_saturday(date = Time.zone.today)
     time_of_day(saturday_shift_start, date)
   end
 
-  def break_start_saturday(date = Date.today)
+  def break_start_saturday(date = Time.zone.today)
     time_of_day(saturday_break_start, date)
   end
 
-  def break_end_saturday(date = Date.today)
+  def break_end_saturday(date = Time.zone.today)
     time_of_day(saturday_break_end, date)
   end
 
-  def shift_end_saturday(date = Date.today)
+  def shift_end_saturday(date = Time.zone.today)
     time_of_day(saturday_shift_end, date)
   end
 
@@ -77,39 +76,23 @@ class Ubs < ApplicationRecord
     break_end_saturday(day).to_i...shift_end_saturday(day).to_i
   end
 
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def times_must_be_ordered
-    if shift_start_date > shift_end_date
-      errors.add(:shift_start, "não pode ser depois do final do expediente")
-    end
+    errors.add(:shift_start, 'não pode ser depois do final do expediente') if shift_start_date > shift_end_date
 
-    if break_start_date > break_end_date
-      errors.add(:break_start, "não pode ser depois do final da pausa")
-    end
+    errors.add(:break_start, 'não pode ser depois do final da pausa') if break_start_date > break_end_date
 
-    if shift_start_date > break_start_date
-      errors.add(:shift_start, "não pode ser depois do início da pausa")
-    end
+    errors.add(:shift_start, 'não pode ser depois do início da pausa') if shift_start_date > break_start_date
 
-    if break_end_date > shift_end_date
-      errors.add(:break_end, "não pode ser depois do final do expediente")
-    end
+    errors.add(:break_end, 'não pode ser depois do final do expediente') if break_end_date > shift_end_date
 
+    errors.add(:saturday_shift_start, 'não pode ser depois do final do expediente') if shift_start_saturday > shift_end_saturday
 
-    if shift_start_saturday > shift_end_saturday
-      errors.add(:saturday_shift_start, "não pode ser depois do final do expediente")
-    end
+    errors.add(:saturday_break_start, 'não pode ser depois do final da pausa') if break_start_saturday > break_end_saturday
 
-    if break_start_saturday > break_end_saturday
-      errors.add(:saturday_break_start, "não pode ser depois do final da pausa")
-    end
+    errors.add(:saturday_shift_start, 'não pode ser depois do início da pausa') if shift_start_saturday > break_start_saturday
 
-    if shift_start_saturday > break_start_saturday
-      errors.add(:saturday_shift_start, "não pode ser depois do início da pausa")
-    end
-
-    if break_end_saturday > shift_end_saturday
-      errors.add(:saturday_break_end, "não pode ser depois do final do expediente")
-    end
-
+    errors.add(:saturday_break_end, 'não pode ser depois do final do expediente') if break_end_saturday > shift_end_saturday
   end
+  # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 end
