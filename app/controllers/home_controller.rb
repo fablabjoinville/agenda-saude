01b6b22186e70a7471862e5ff93b60c3d47c26d4
Julio Monteiro <julio@monteiro.eti.br>
2021-05-09 00:00:48 -0300

    Usar cache store

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,32 @@
 class HomeController < ApplicationController
-  # rubocop:disable Metrics/AbcSize
+  CACHE_EXPIRES_IN = 30.seconds
+
   def index
     return redirect_to home_community_appointments_path if current_patient
     return redirect_to operator_ubs_index_path if current_user
 
+    @can_register_condition_names = Rails.cache.fetch('home/condition_names', expires_in: CACHE_EXPIRES_IN) do
+      condition_names
+    end
+    @can_schedule_conditions = Rails.cache.fetch('home/schedule_conditions', expires_in: CACHE_EXPIRES_IN) do
+      schedule_conditions
+    end
+
+    @appointments_any = @can_schedule_conditions.pluck(:doses_count).inject(:+)&.positive?
+  end
+
+  protected
+
+  def condition_names
+    Condition.active.can_register.order(:name).pluck(:name)
+  end
+
+  def schedule_conditions
     from = Rails.configuration.x.schedule_from_hours.hours.from_now
     to = Rails.configuration.x.schedule_up_to_days.days.from_now.end_of_day
     ubs_count = Ubs.count
 
-    @can_register_condition_names = Condition.active.can_register.order(:name).pluck(:name)
-    @can_schedule_conditions = Condition.active.can_schedule.order(:name).includes(:ubs).map do |c|
+    Condition.active.can_schedule.order(:name).includes(:ubs).map do |c|
       {
         name: c.name,
         ubs_names: c.ubs_ids.count == ubs_count ? 'todas unidades' : c.ubs.order(:name).pluck(:name).join(', '),
@@ -18,8 +35,5 @@ class HomeController < ApplicationController
                                 .count
       }
     end
-
-    @appointments_any = @can_schedule_conditions.pluck(:doses_count).inject(:+)&.positive?
   end
-  # rubocop:enable Metrics/AbcSize
 end
