dc0199b1d2d1faea0de12c14b236ca385596f841
Julio Monteiro <julio@monteiro.eti.br>
2021-05-08 22:30:49 -0300

    Fix migration

--- db/migrate/20210508021843_change_default_saturday_shift_defaults_on_ubs.rb
+++ db/migrate/20210508021843_change_default_saturday_shift_defaults_on_ubs.rb
@@ -1,10 +1,15 @@
 class ChangeDefaultSaturdayShiftDefaultsOnUbs < ActiveRecord::Migration[6.1]
   def change
-    change_table :ubs, bulk: true do |t|
-      t.string :saturday_shift_start, null: true, default: nil
-      t.string :saturday_break_start, null: true, default: nil
-      t.string :saturday_break_end, null: true, default: nil
-      t.string :saturday_shift_end, null: true, default: nil
-    end
+    change_column_null :ubs, :saturday_shift_start, true
+    change_column_default :ubs, :saturday_shift_start, from: "9:00", to: nil
+
+    change_column_null :ubs, :saturday_break_start, true
+    change_column_default :ubs, :saturday_break_start, from: "12:30", to: nil
+
+    change_column_null :ubs, :saturday_break_end, true
+    change_column_default :ubs, :saturday_break_end, from: "13:30", to: nil
+
+    change_column_null :ubs, :saturday_shift_end, true
+    change_column_default :ubs, :saturday_shift_end, from: "17:00", to: nil
   end
 end
