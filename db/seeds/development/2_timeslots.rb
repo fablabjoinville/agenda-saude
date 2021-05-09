92f845b476cc23244d1c4e93cf89751c3294deb4
Julio Monteiro <julio@monteiro.eti.br>
2021-05-08 21:59:39 -0300

    Fix cypress test

--- db/seeds/development/2_timeslots.rb
+++ db/seeds/development/2_timeslots.rb
@@ -1,6 +1,3 @@
-from = 15.minutes.from_now.to_datetime
-to = 7.days.from_now.to_datetime
-
 Ubs.all.each do |ubs|
-  TimeSlotGenerationService.new.call(ubs: ubs, from: from, to: to)
+  TimeSlotGenerationService.new.call(ubs: ubs, from: Time.zone.today, to: 4.days.from_now.to_date)
 end
