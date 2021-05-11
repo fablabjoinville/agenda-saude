285089d05f7aaaf512afff2810202875d0bf2699
Julio Monteiro <julio@monteiro.eti.br>
2021-05-11 00:20:44 -0300

    Melhorar cache para Pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,6 @@
 class Page < ApplicationRecord
+  CACHE_TTL = 5.minutes # just to make sure our cache is regularly clean
+
   enum context: {
     embedded: 0,
     user_created: 1
@@ -10,6 +12,8 @@ class Page < ApplicationRecord
   scope :admin_order, -> { order(:context, :path) }
 
   def html
-    Kramdown::Document.new(body).to_html.html_safe # rubocop:disable Rails/OutputSafety
+    Rails.cache.fetch("#{cache_key_with_version}/html", expires_in: CACHE_TTL) do
+      Kramdown::Document.new(body).to_html.html_safe # rubocop:disable Rails/OutputSafety
+    end
   end
 end
