d33761bab531da3462762a801af3dd9884e4be73
Julio Monteiro <julio@monteiro.eti.br>
2021-05-13 22:16:41 -0300

    Interface simples de admin para operadores (#324)
    * Interface simples de admin para operadores
    
    * Readme

--- app/controllers/admin/patients_controller.rb
+++ app/controllers/admin/patients_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class PatientsController < Base
     before_action :set_patient, only: %i[show unblock]
+    skip_before_action :require_administrator!, only: %i[index show unblock]
 
     FILTERS = {
       search: 'search',
