c6a9e1eac11f398f8ac3f50679315c02ef59d663
Julio Monteiro <julio@monteiro.eti.br>
2021-06-08 10:36:58 -0300

    Rate Limiter

--- app/controllers/community/base.rb
+++ app/controllers/community/base.rb
@@ -1,5 +1,7 @@
 module Community
   class Base < ApplicationController
+    include RateLimited
+
     prepend_before_action :authenticate!
 
     protected
@@ -7,6 +9,10 @@ module Community
     def authenticate!
       if current_patient
         Appsignal.tag_request(patient_id: current_patient.id)
+
+        return rate_limit! if exceeded_rate_limit?
+
+        record_on_rate_limit
 
         return current_patient
       end
---
+++ app/controllers/concerns/rate_limited.rb
@@ -0,0 +1,22 @@
+module RateLimited
+  extend ActiveSupport::Concern
+
+  def rate_limit!
+    render plain: I18n.t('alerts.rate_limit_exceeded'), status: :too_many_requests
+  end
+
+  def exceeded_rate_limit?
+    RedisProvider.redis.get(rate_limit_key).to_i > Rails.configuration.x.rate_limit_per_minute
+  end
+
+  def rate_limit_key
+    @rate_limit_key ||= ['rl', current_patient.id, Time.zone.now.strftime('%M')].join(':')
+  end
+
+  def record_on_rate_limit
+    RedisProvider.redis.multi do
+      RedisProvider.redis.incr rate_limit_key
+      RedisProvider.redis.expire rate_limit_key, 59
+    end
+  end
+end
