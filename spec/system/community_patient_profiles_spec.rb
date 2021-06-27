b85ad192bb29142520203f980d94a64303cc2bc5
Julio Monteiro <julio@monteiro.eti.br>
2021-06-27 14:48:43 -0300

    Testing firefox out

--- spec/system/community_patient_profiles_spec.rb
+++ spec/system/community_patient_profiles_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Patients managing their profiles", type: :system do
-  # before do
-  #   driven_by(:rack_test)
-  # end
+  before do
+    # driven_by(:rack_test)
+    driven_by :selenium, using: :firefox
+  end
 
   scenario "sign up with a new patient" do
     visit root_path
