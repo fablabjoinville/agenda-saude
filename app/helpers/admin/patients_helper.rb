6c6db4b959f3e3c1d4e4c3f92c6ea4653770b050
jamessom <jamessomqueiroz@gmail.com>
2021-10-21 00:05:31 -0300

    Bloquei botão quando existe dose cadastrada (#371)

--- app/helpers/admin/patients_helper.rb
+++ app/helpers/admin/patients_helper.rb
@@ -14,5 +14,15 @@ module Admin
         links << search_link(current_search, total_count, admin_patients_path(search: current_search)) if current_search.present?
       end.join.html_safe # rubocop:disable Rails/OutputSafety
     end
+
+    def register_appointment(patient)
+      link_class = 'btn btn-success'
+
+      if patient.doses.any?
+        link_class = 'btn btn-success disabled'
+      end
+
+      link_to "Registrar dose", new_admin_appointment_path(patient_id: patient.id), class: link_class
+    end
   end
 end
